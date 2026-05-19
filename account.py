02b8ae5802666c78aaf80d63e91f57f80074e0be
Abu Toasin Oakil <oakil@kth.se>
2026-05-19 10:06:17 +0200

    feat: add transaction logging to Account

--- account.py
+++ account.py
@@ -3,12 +3,24 @@ class Account:
         self.name = name
         self.balance = 0.0
         self.currency = currency
+        # add empty transaction list to store transaction history
+        self.transactions = []  # list of dicts
 
     def deposit(self, amount):
         self.balance += amount
+        self._log("deposit", amount)
 
     def withdraw(self, amount):
         self.balance -= amount
+        self._log("withdraw", amount)
+    # logging transactions
+    def _log(self, type, amount):
+        entry = {
+            "type": type,
+            "amount": amount,
+            "balance_after": self.balance
+        }
+        self.transactions.append(entry)
 
     def get_balance(self):
         return self.balance
