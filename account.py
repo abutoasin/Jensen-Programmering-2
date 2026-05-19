fcfa08ad055dc0bfe98dfc74aedc2a642a540157
Abu Toasin Oakil <oakil@kth.se>
2026-05-19 11:22:03 +0200

    refactor: add error handling with try/except in Account methods

--- account.py
+++ account.py
@@ -6,14 +6,31 @@ class Account:
         # add empty transaction list to store transaction history
         self.transactions = []  # list of dicts
 
+    # Method deposit
     def deposit(self, amount):
-        self.balance += amount
-        self._log("deposit", amount)
+        try:
+            if amount <= 0:
+                raise ValueError("Amount must be positive.")
+            self.balance += amount
+            self._log("deposit", amount)
+        
+        except Exception as e:
+            print(f"Amount error: {e}")
 
+    # Method withdraw    
     def withdraw(self, amount):
-        self.balance -= amount
-        self._log("withdraw", amount)
-    # logging transactions
+        try:
+            if amount<= 0:
+                raise ValueError("Amount must be positive.")
+            if amount > self.balance:
+                raise ValueError("Insufficient funds!")
+            self.balance -= amount
+            self._log("withdraw", amount)
+        
+        except Exception as e:
+            print(f"Amount error: {e}")
+    
+    # Method logging transactions
     def _log(self, type, amount):
         entry = {
             "type": type,
@@ -22,5 +39,6 @@ class Account:
         }
         self.transactions.append(entry)
 
+    # Method show balance
     def get_balance(self):
         return self.balance
